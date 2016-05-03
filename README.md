6d010b2baa7ed79438f551b79806810b267fce9b
Ivan Diesel <dieselpardal@gmail.com>
2016-05-03 14:53:44 -0300

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Entregas
+
+Projeto de Entregas 1.0
+Programador: Ivan Diesel
+Data: 03/05/2016
+
+
+O ambiente de projeto de Entregas 1.0 na ambiente OSX com visualização de biblioteca D3.
+Os pacotes mencionados do documento podem ser encontrados na mesma pasta deste arquivo.
--- out/production/Entregas/main/service.js
+++ out/production/Entregas/main/service.js
@@ -1,133 +1,87 @@
 
 var svg;
-var mouX = 0;
-var mouY = 0;
-function circulo(x,y,r,cor,client,ligar,name) {
+var width = 500;
+var height = 500;
+
+function circle(x,y,color,client,join,name) {
   svg.append('circle')
       .attr('class', 'origin')
       .attr('cx',x)
       .attr('cy',y)
-      .attr('r', r)
-      .attr('fill',cor)
+      .attr('r', 10)
+      .attr('fill',color)
       .style('opacity', 0.3)
-      .style('stroke-width',"3px")
       .on("mouseover", function (d) {
-                                      apontar(client,ligar,name);
+                                      indicate(client,join,name);
                                     });
-};
+}
 
-function apontar (client,ligar,name) {
-reta(0,0,500,500,'Yellow');
- criarGrafos();
-    for(var g=0; g<ligar.length; g++) {
-            if (name == ligar[g].origem.name) {
-
-             line(ligar[g].origem.x,ligar[g].origem.y,
-                  ligar[g].destino.x,ligar[g].destino.y,
-                  'red' );
-
-            }
-
-        }
-
-};
-function line(lx1,ly1,lx2,ly2,cor) {
-  svg.append('line')
-      .attr('class', 'arrow')
-      .attr('x1', lx1)
-      .attr('x2', lx2)
-      .attr('y1', ly1)
-      .attr('y2', ly2)
-      .attr('stroke',cor)
-      ;
-
-};
-
-function reta(x,y,l,a,cor) {
+function clearScreen() {
   svg.append('rect')
-    .attr('x', x)
-    .attr('y', y)
-    .attr('width', l)
-    .attr('height', a)
-    .style('fill',cor);
-};
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', width)
+    .attr('height', height)
+    .style('fill','yellow');
+}
 
-this.escrever = function(texto,x,y,largura,fonteFamilia,fonteSize,cor) {
+function print(text,x,y,width,fontFamily,fontSize,color) {
   svg.append('text')
     .attr('x', x)
     .attr('y', y)
-    .attr('width', largura)
-    .style("font-size", fonteSize)
-    .style('fill',cor)
-    .style('font-family',fonteFamilia)
+    .attr('width', width)
+    .style("font-size", fontSize)
+    .style('fill',color)
+    .style('font-family',fontFamily)
     .style("text-anchor", "beginning")
-    .text(texto);
-};
-function criarGrafos(client,ligar) {
-for(var f=0; f<client.length;f++)
-        {
-                circulo(client[f].x,client[f].y,5,'black',client,ligar,client[f].name);
-                escrever(client[f].name,client[f].x,client[f].y, 10,'arial',12,'black');
+    .text(text);
+}
+
+function mean(origin,destiny) {
+ return (origin + destiny) /2;
+}
+
+function indicate(client,join,name) {
+ clearScreen();
+ createGraph(client,join);
+    for(var g=0; g<join.length; g++) {
+            if (name == join[g].origin.name) {
+             line(join[g].origin.x,join[g].origin.y,
+                  join[g].destiny.x,join[g].destiny.y,
+                  'red' );
+             print(join[g].cento+"%",
+                   mean(join[g].origin.x, join[g].destiny.x),
+                   mean(join[g].origin.y, join[g].destiny.y),
+                   10,
+                   'arial',
+                   12,
+                   'blue');
+            }
         }
 }
-function gerarGrafico() {
 
-init();
-reta(0,0,500,500,'Yellow');
+function line(x1,y1,x2,y2,color) {
+  svg.append('line')
+      .attr('class', 'arrow')
+      .attr('x1', x1)
+      .attr('y1', y1)
+      .attr('x2', x2)
+      .attr('y2', y2)
+      .style('stroke-width',"3px")
+      .attr('stroke',color);
+}
 
+function createGraph(client,join) {
+    for(var f=0; f<client.length;f++)
+            {
+                    circle(client[f].x,client[f].y,'black',client,join,client[f].name);
+                    print(client[f].name,client[f].x,client[f].y, 10,'arial',12,'black');
+                    print(client[f].qtd,client[f].x,client[f].y+20, 10,'arial',12,'black');
+            }
+}
 
-var client = [
-        {
-            "name": "Ivan",
-            "x":50,
-            "y":50,
-            "qtd": 50
-        },
-        {
-             "name": "Clara",
-             "x":200,
-             "y":50,
-             "qtd": 100
-        }
-        ,
-        {
-             "name": "Gustavo",
-             "x":50,
-             "y":200,
-             "qtd": 150
-        }
-];
-var ligar = [
-        {
-              "origem": client[0],
-              "destino":client[1],
-              "cento":50
-        },
-        {
-              "origem": client[1],
-              "destino":client[2],
-              "cento":40
-        }
-        ,
-        {
-              "origem": client[0],
-              "destino":client[2],
-              "cento":90
-        }
-    ];
-criarGrafos();
-};
 function init() {
-//alert("AAAAAAAAA");
-var margem = {top: 0, right: 0, bottom: 0, left: 0};
-    svg = d3.select(document.getElementById('quadros')).append('svg')
-         .attr('width', 500)
-         .attr('height', 500)
-         .append('g')
-         .attr('transform', 'translate(' + margem.left + ',' + margem .top + ')')
-         .on("mousemove", function(event) {
-                         coordinates = d3.mouse(this);
-                         mouX = coordinates[0];
-                         mouY = coordinates[1];} );
-//reta(svg,0,0,500,500,'red');
-         }
+        svg = d3.select(document.getElementById('quadros')).append('svg')
+             .attr('width', width)
+             .attr('height', height) ;
+}
